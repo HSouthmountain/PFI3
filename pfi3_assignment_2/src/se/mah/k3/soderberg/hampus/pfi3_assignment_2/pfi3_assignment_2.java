f423f16c9e9408f59765cb4c2edebd900020c146
Hampe <Hampe@BIANCA>
2011-04-01 10:46:17 +0200

    Signed-off-by: Hampe <Hampe@BIANCA>

--- pfi3_assignment_2/src/se/mah/k3/soderberg/hampus/pfi3_assignment_2/pfi3_assignment_2.java
+++ pfi3_assignment_2/src/se/mah/k3/soderberg/hampus/pfi3_assignment_2/pfi3_assignment_2.java
@@ -1,10 +1,10 @@
-package se.mah.k3.soderberg.hampus.pfi3_assignment_1;
+package se.mah.k3.soderberg.hampus.pfi3_assignment_2;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class pfi3_assignment_1 extends Activity {
-    /** Called when the activity is first created. Nmsndskdhjsdh */
+public class pfi3_assignment_2 extends Activity {
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
