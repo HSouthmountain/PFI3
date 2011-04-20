4df7e7790f612e9c5e6bce99f6d2a4283d3e9478
hampus <hampus@BIANCA>
2011-04-20 18:29:16 +0200

    Signed-off-by: hampus <hampus@BIANCA>

---
+++ EssEmEssV1/src/se/mah/k3/soderberg/hampus/EssEmEssV1/EssEmEssV1.java
@@ -0,0 +1,62 @@
+package se.mah.k3.soderberg.hampus.EssEmEssV1;
+
+import se.k3.goransson.andreas.essemmesslib.Essemmess;
+import se.k3.goransson.andreas.essemmesslib.EssemmessHelper;
+import se.k3.goransson.andreas.essemmesslib.EssemmessLoginEvent;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class EssEmEssV1 extends Activity implements OnClickListener {
+
+	Essemmess server = EssemmessHelper.getServer(this);
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button loginButton = (Button) findViewById(R.id.button1);
+        loginButton.setOnClickListener(this);
+    }
+
+	@Override
+	public void onClick(View v) {
+        EditText usernameText = (EditText) findViewById(R.id.editText1);
+        EditText passwordText = (EditText) findViewById(R.id.editText2);
+		switch (v.getId()) {
+			case R.id.button1:
+				//INLOGGNINGEN SKALL FINNAS MED!!
+				server.login(usernameText.getText().toString(), passwordText.getText().toString());
+				
+				//TEMP:
+				//Intent messages = new Intent(EssEmEssV1.this, Messages.class); //Byter activity
+				//startActivity(messages);									//Byter activity
+				//TEMP
+				
+				
+				break;
+		}
+		
+	}
+	
+	public void NewEssemmessLogin(EssemmessLoginEvent evt) {
+		//**************************
+		if (evt.getLoggedin() == true) {   //ÄNDRA TILL TRUE!!
+			Intent messages = new Intent(EssEmEssV1.this, Messages.class); //Byter activity
+			startActivity(messages);										//Byter activity
+		}
+	}
+	
+protected void onStop(){
+	Log.i("bla", "ONSTOP");
+	//LOGGA UT HÄR!!! (tror jag! ;))
+}
+}
---
+++ EssEmEssV1/src/se/mah/k3/soderberg/hampus/EssEmEssV1/Messages.java
@@ -0,0 +1,49 @@
+package se.mah.k3.soderberg.hampus.EssEmEssV1;
+
+import se.k3.goransson.andreas.essemmesslib.Essemmess;
+import se.k3.goransson.andreas.essemmesslib.EssemmessHelper;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+
+public class Messages extends Activity implements OnClickListener {
+    
+	Essemmess server = EssemmessHelper.getServer(this);
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.messages);
+        
+    }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		EditText sendText = (EditText) findViewById(R.id.editTextMessageToSend);
+        EditText getText = (EditText) findViewById(R.id.editTextMessageReceivedSent);
+        EditText sendTag = (EditText) findViewById(R.id.editTextMessageToSend);
+		
+        
+        switch (v.getId()) {
+			case R.id.buttonSend:
+				
+				//Skicka meddelande
+				server.post(sendText.getText().toString(), "test");
+				
+				
+				break;
+				
+			case R.id.buttonGet:
+				
+				//Hämta meddelande + mitt
+			server.read("test");
+				break;
+		}
+		
+	}
+    
+}
